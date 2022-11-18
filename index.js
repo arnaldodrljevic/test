8bfc6286067bf813a8e6f5030b25457f2aaa5b4b
unknown <arnaldo.drljevic@gmail.com>
2022-11-18 20:18:44 +0100

    Verzija 18_11_22

---
+++ index.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "Garfield",
+    Surname: "John",
+    age: 22
+
+};
+
+let personAge = person.age;
+
+console.log("Ime " + person.name);
+
+let prezime = person['Surname'];
+console.log(prezime);
